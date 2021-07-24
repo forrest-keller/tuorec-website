8cad76847cc80b1418fbf6198f84fe6475a25091
Forrest Keller <forrestblackburnkeller@gmail.com>
2021-07-24 14:22:43 -0600

    Integrate Chakra UI base

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,13 @@
+import { ChakraProvider } from "@chakra-ui/react";
 import { AppProps } from "next/app";
+import theme from "utilities/theme";
 
 const CustomApp = ({ Component, pageProps }: AppProps): JSX.Element => {
-  return <Component {...pageProps} />;
+  return (
+    <ChakraProvider theme={theme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
 };
 
 export default CustomApp;
