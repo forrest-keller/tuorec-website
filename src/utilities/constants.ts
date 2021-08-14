b07482792edc0a5a4d27e57d5e48ddc0861597ae
Forrest Keller <forrestblackburnkeller@gmail.com>
2021-08-14 14:44:07 -0600

    Add page to redirect to GraphCMS

--- src/utilities/constants.ts
+++ src/utilities/constants.ts
@@ -1,11 +1,13 @@
 const graphCmsEndpointUrl = process.env.NEXT_PUBLIC_GRAPH_CMS_ENDPOINT_URL;
 const rentalFormUrl = process.env.NEXT_PUBLIC_RENTAL_FORM_URL;
+const editUrl = process.env.NEXT_PUBLIC_EDIT_URL;
 
-if (!graphCmsEndpointUrl || !rentalFormUrl) {
+if (!graphCmsEndpointUrl || !rentalFormUrl || !editUrl) {
   throw new Error("Required environment variables not supplied.");
 }
 
 export const constants = {
-  graphCmsEndpointUrl: graphCmsEndpointUrl,
-  rentalFormUrl: rentalFormUrl,
+  graphCmsEndpointUrl,
+  rentalFormUrl,
+  editUrl,
 };
